5a4defa1f6a7131ff4ee30f8146393b28d915f6d
Tayyaba Riasat <tayyabariasat1999@gmail.com>
2024-10-07 23:05:38 -0400

    minor changes

--- palindrome string/index.js
+++ palindrome string/index.js
@@ -1,45 +1,51 @@
-var year = new Date().getFullYear();
-document.getElementById('copyright-year').innerHTML=year;
+// getting userinput input field by id
+const inputString = document.getElementById('userString');
 
+// getting area to show text by id
+const display = document.getElementById('displayText');
 
-      const inputString = document.getElementById('userString');
-      const display = document.getElementById('displayText');
+document.getElementById("userString").addEventListener("keyup", function(event) 
+  {
+    event.preventDefault();
+
+    if (event.keyCode === 13) 
+    {
+      document.getElementById("myButton").click();
+    }
+});
+
+function check()
+{
+  var inpString = inputString.value;
+  if (inpString===""){
+
+      alert("Nothing entered the application will go back to home page.");
+      window.location.href = "..\\home.html";
+  }
+  else
+  {
+    
+// removing characters from input string and converting into lowercase
+      var reg = /[\W_]/g;
+      var sub_String = inpString.toLowerCase().replace(reg,"");
+// reversing the input string
+      var revered = sub_String.split("").reverse().join("");
       
-      document.getElementById("userString").addEventListener("keyup", function(event) 
-        {
-          event.preventDefault();
-
-          if (event.keyCode === 13) 
-          {
-           document.getElementById("myButton").click();
-          }
-      });
-
-      function myfunction()
-      {
-        var inpString = inputString.value;
-        if (inpString===""){
-
-           alert("Nothing entered the application will go back to home page.");
-           window.location.href = "..\\home.html";
-        }
-        else
-        {
-           var reg = /[\W_]/g;
-           var sub_String = inpString.toLowerCase().replace(reg,"");
-           var revered = sub_String.split("").reverse().join("");
-           if (revered=== sub_String) {
-            document.getElementById('correctanswer').play();
-            display.innerHTML = "You entered: " + inputString.value + "\n Yes, the above string is a Palindrome.";
-            
-           }
-           
-           else{
-            document.getElementById('wronganswer').play();
-
-            display.innerHTML = "You entered: " + inputString.value + "\n No, the above string is not a Palindrome.";
-            
-           }
-        }
+// checking if input and reversed string are same
+      if (revered=== sub_String) {
+// play audio
+      document.getElementById('correctanswer').play();
+      //display result
+      display.innerHTML = "You entered: " + inputString.value + "\n Yes, the above string is a Palindrome.";
+      
       }
+      
+      else{
+      document.getElementById('wronganswer').play();
+
+      display.innerHTML = "You entered: " + inputString.value + "\n No, the above string is not a Palindrome.";
+      
+      }
+  }
+}
       
